2fbaaff49cffc83d56592e872ec9ba312557f442
Mantas Banevičius <mantas.bane@gmail.com>
2021-02-01 21:40:07 +0200

    Created camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject _player;
+    private Vector3 _offset;
+    void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _offset = transform.position - _player.transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        var position = transform.position;
+        var followPoint = new Vector3(_player.transform.position.x+_offset.x, position.y, _player.transform.position.z+_offset.z);
+        position = Vector3.Lerp(position, followPoint, 0.1f);
+        transform.position = position;
+    }
+}
