a2d63cfd70b4971f2c5f16849e60ded6b0a048e0
Mantas Banevičius <mantas.bane@gmail.com>
2021-02-02 23:18:57 +0200

    Added bullet functionality and collision detection

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private float _speed = 10;
+    public LayerMask collisionMask;
+    private float _damage = 1;
 
     public void SetBulletSpeed(float speed)
     {
@@ -14,7 +16,30 @@ public class Bullet : MonoBehaviour
 
     private void Update()
     {
-        transform.Translate(Vector3.forward * (Time.deltaTime * _speed));
+        float moveDistance = _speed * Time.deltaTime;
+        CheckCollisions(moveDistance);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    private void CheckCollisions(float moveDistance)
+    {
+        var ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    void OnHitObject(RaycastHit hit)
+    {
+        IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeHit(_damage, hit);
+        }
+        Destroy(gameObject);
     }
 
     private void Start()
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour, IDamageable
+{
+    private NavMeshAgent _navMeshAgent;
+    private Transform _player;
+
+    public float _health = 3.0f;
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update()
+    {
+        _navMeshAgent.SetDestination(_player.position);
+    }
+
+    public void TakeHit(float damage, RaycastHit hit)
+    {
+        _health -= damage;
+
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
