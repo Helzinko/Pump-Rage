4a434ad85bba78367d9ee15373bd84985f4fc0f4
Mantas Banevičius <mantas.bane@gmail.com>
2021-02-02 19:51:45 +0200

    Added bend tree and player animations.
    Idle, Run Aim Forward, Run Aim Backward,  Run Aim Left,  Run Aim Right,

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,17 +12,51 @@ public class Player : MonoBehaviour
 
     private Plane _groundObject;
 
+    private Animator _animator;
+    private static readonly int Turn = Animator.StringToHash("Turn");
+    private static readonly int Forward = Animator.StringToHash("Forward");
+
+    private Transform _cam;
+    private Vector3 _camForward;
+    private Vector3 _move;
+    private Vector3 _moveInput;
+    private float _forwardAmount;
+    private float _turnAmount;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
         _camera = Camera.main;
         _groundObject = new Plane(Vector3.up, Vector3.zero);
+
+        _animator = GetComponent<Animator>();
+        _cam = _camera.transform;
     }
 
     void Update()
     {
-        _playerVelocity = (new Vector3(Input.GetAxisRaw("Horizontal"), 
-            0, Input.GetAxisRaw("Vertical"))).normalized * movementSpeed;
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        if (_cam != null)
+        {
+            _camForward = Vector3.Scale(_cam.up, new Vector3(1, 0, 1)).normalized;
+            _move = vertical * _camForward + horizontal * _cam.right;
+        }
+        else
+        {
+            _move = vertical * Vector3.forward + horizontal * Vector3.right;
+        }
+
+        if (_move.magnitude > 1)
+        {
+            _move.Normalize();
+        }
+
+        Move(_move);
+        
+        _playerVelocity = (new Vector3(horizontal, 
+            0, vertical)).normalized * movementSpeed;
 
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
@@ -32,7 +66,33 @@ public class Player : MonoBehaviour
         var lookPoint = new Vector3(point.x, transform.position.y, point.z);
         transform.LookAt(lookPoint);
     }
-    
+
+    private void Move(Vector3 move)
+    {
+        if (move.magnitude > 1)
+        {
+            move.Normalize();
+        }
+
+        this._moveInput = move;
+
+        ConvertMoveInput();
+        UpdateAnimator();
+    }
+
+    private void UpdateAnimator()
+    {
+        _animator.SetFloat(Turn, _forwardAmount, 0.1f, Time.deltaTime);
+        _animator.SetFloat(Forward, _turnAmount, 0.1f, Time.deltaTime);
+    }
+
+    private void ConvertMoveInput()
+    {
+        var localMove = transform.InverseTransformDirection(_moveInput);
+        _turnAmount = localMove.x;
+        _forwardAmount = localMove.z;
+    }
+
     private void FixedUpdate()
     {
         _rigidbody.MovePosition(_rigidbody.position + _playerVelocity * Time.fixedDeltaTime);
