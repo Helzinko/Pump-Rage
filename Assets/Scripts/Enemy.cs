bc2a41ba832881f29b2a675a4cb11e2fb59d268a
Mantas Banevičius <mantas.bane@gmail.com>
2021-02-03 19:46:17 +0200

    Added first enemy model and his running and idle animations.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,48 @@ using UnityEngine.AI;
 [RequireComponent (typeof (NavMeshAgent))]
 public class Enemy : MonoBehaviour, IDamageable
 {
+    public enum State
+    {
+        Idle,
+        Chasing,
+        Attacking,
+        Dead
+    }
+
+    private State _currentState;
+    
     private NavMeshAgent _navMeshAgent;
     private Transform _player;
+
+    private float _attackDistance = 5f;
+    private float _timeBetweenAttacks = 1;
+    private float _nextAttackTime;
 
     public float _health = 3.0f;
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        _currentState = State.Chasing;
     }
     
     void Update()
     {
-        _navMeshAgent.SetDestination(_player.position);
-    }
+        if(_currentState == State.Chasing)
+            _navMeshAgent.SetDestination(_player.position);
 
+        if (Time.time > _nextAttackTime)
+        {
+            float sqrDistanceToPlayer = (_player.position - transform.position).sqrMagnitude;
+            if (sqrDistanceToPlayer < Mathf.Pow(_attackDistance, 2))
+            {
+                _nextAttackTime = Time.time + _timeBetweenAttacks;
+                Debug.Log("Attack");
+            }
+        }
+    }
+    
     public void TakeHit(float damage, RaycastHit hit)
     {
         _health -= damage;
